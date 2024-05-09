84a7d58f9e0aa4ca644d0d582b0f7e8c67d40cdf
Carlos Eduardo <ducameneses16@gmail.com>
2024-05-09 01:23:40 -0300

    feat: create models and adjust schemas, tests

--- store/models/base.py
+++ store/models/base.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timezone
+from decimal import Decimal
+from typing import Any
 import uuid
-from pydantic import UUID4, BaseModel, Field
+from bson import Decimal128
+from pydantic import UUID4, BaseModel, Field, model_serializer
 
 
 def datetime_now() -> datetime:
@@ -11,3 +14,13 @@ class CreateBaseModel(BaseModel):
     id: UUID4 = Field(default_factory=uuid.uuid4)
     created_at: datetime = Field(default_factory=datetime_now)
     updated_at: datetime = Field(default_factory=datetime_now)
+
+    @model_serializer
+    def set_model(self) -> dict[str, Any]:
+        self_dict = dict(self)
+
+        for key, value in self_dict.items():
+            if isinstance(value, Decimal):
+                self_dict[key] = Decimal128(str(value))
+
+        return self_dict
--- tests/usecases/test_product.py
+++ tests/usecases/test_product.py
@@ -41,7 +41,7 @@ async def test_usecases_get_all_should_return_success():
 
 
 async def test_usecases_update_should_return_success(product_up, product_inserted):
-    product_up.price = 7.500
+    product_up.price = "7.500"
     result = await product_usecase.update(id=product_inserted.id, body=product_up)
 
     assert isinstance(result, ProductUpdateOut)
